89ca8ac12fa5cbafe02543e6767aba0699d24f91
= <=>
2025-06-03 20:05:31 +0000

    nuxt.config.ts modifier

--- frontend/smart_search/nuxt.config.ts
+++ frontend/smart_search/nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     // Variables publiques (accessibles côté client et serveur)
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000'
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'https://smartsearch.myfad.org'
     }
   },
 
